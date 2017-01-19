faccfd4257cd1fd4aa0d538b9f5f9e875253bd53
zj974185723 <974185723@qq.com>
2017-01-19 15:52:33 +0800

    行情获取class单独出来

---
+++ api-common-util/src/main/java/com/zj/api/common/util/quote/QuoteUtil.java
@@ -0,0 +1,17 @@
+package com.zj.api.common.util.quote;
+
+
+import com.zj.api.model.stock.QuoteInfo;
+
+import java.util.Map;
+
+/**
+ * Created by js on 2017/1/16.
+ */
+public interface QuoteUtil {
+
+    QuoteInfo getStockQuote(String stockCode);
+
+    Map<String, QuoteInfo> getStocksQuote(String stockCodes);
+
+}
