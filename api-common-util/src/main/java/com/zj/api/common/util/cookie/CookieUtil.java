e8f891ae76e5fde037f49110cdff1ba55db151e7
zj974185723 <974185723@qq.com>
2017-01-25 17:08:45 +0800

    添加cookie方法

--- api-common-util/src/main/java/com/zj/api/common/util/cookie/CookieUtil.java
+++ api-common-util/src/main/java/com/zj/api/common/util/cookie/CookieUtil.java
@@ -4,6 +4,7 @@ import org.springframework.util.StringUtils;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by js on 2017/1/25.
@@ -11,17 +12,39 @@ import javax.servlet.http.HttpServletRequest;
 public class CookieUtil {
 
 
-    public static String getCookie(HttpServletRequest httpServletRequest, String cookieName) {
-        if (httpServletRequest == null || !StringUtils.hasText(cookieName)) {
+    public static String getCookie(HttpServletRequest request, String cookieName) {
+        if (request == null || !StringUtils.hasText(cookieName)) {
             throw new IllegalArgumentException("参数不能为空");
         }
-        Cookie[] cookies = httpServletRequest.getCookies();
+        Cookie[] cookies = request.getCookies();
         for (Cookie cookie : cookies) {
             if (cookie.getName().equals(cookieName)) {
                 return cookie.getValue();
             }
         }
         return null;
+    }
+
+    /**
+     * 添加cookie
+     *
+     * @param response
+     * @param name     名称
+     * @param domain   域名
+     * @param value    值
+     * @param expiry   时间
+     */
+    public static void setCookie(HttpServletResponse response, String name, String domain, String value, int expiry) {
+        if (response == null || StringUtils.isEmpty(name) || StringUtils.isEmpty(value)) {
+            throw new IllegalArgumentException("参数不能为空");
+        }
+        Cookie cookie = new Cookie(name, value);
+        if (StringUtils.hasText(domain)) {
+            cookie.setDomain(domain);
+        }
+        cookie.setMaxAge(expiry);
+        cookie.setPath("/");
+        response.addCookie(cookie);
     }
 
 }
